195e87ae46fa7711d4f4b1d07951105de630f0d5
anjak610 <anja.siegrid.kretschmer@hs-furtwangen.de>
2017-02-07 14:01:45 +0100

    In Geogebra
    added calculation of depth magnification, width magnification, horizontal shifting of image sensor and shape ratio

--- Shape_Ratio.m
+++ Shape_Ratio.m
@@ -1,18 +1,18 @@
 % Defining an Objekt in Scene with width and depth
 x0 = 200;  %x-coord of object in mm
-z0 = 300;  %z-coord of object in mm
-W0 = 300;  % x0b - x0a distance between two edges a and b of the object in mm
+z0 = 100;  %z-coord of object in mm
+W0 = 100;  % x0b - x0a distance between two edges a and b of the object in mm
 
 % Values are picked out of S3DKalkulator
 f = 36; % Focallength in mm
-t = 136; % Stereobase in mm (distance of image sensors)
+t = 102; % Stereobase in mm (distance of image sensors) --> Stereobasis? oder Sensorbreite?
 Ws = 4040; % in mm --> viewing screen width
 e = 55; % Distance between eyes
-V = 4100; % Distance between eyes and screen
+V = 5300; % Distance between eyes and screen
 
 % Compute h with given Valus --> Hit Prewiev is given in Pixel
-HIT = 37; % in Pixel
-Wc = 22.3; % in mm --> Image sensor Width
+HIT = 39; % in Pixel
+Wc = 20.1; % in mm --> Image sensor Width
 xres = 1920; % Resolution --> Other possible Values are 720, 1280, 2048, 4096
 h= (HIT * Wc)/ xres; % Get h shift out of HIT_Pixel (derived after h)
 
@@ -30,8 +30,8 @@ zi = (V*e*z0)/(M*f*t-z0*(2*M*h-e));
 
 %Compute Width Magnification --> first part for computing Shape Ratio
 Wi3D = (M*e*f*W0)/(M*f*t-z0*(2*M*h-e));
-Mi3D = Wi3D/W0; % Width Magnification;
-%Mi3D = (M*e*f)/(M*f*t-z0*(2*M*h-e));
+%Mi3D = Wi3D/W0; % Width Magnification;
+Mi3D = (M*e*f)/(M*f*t-z0*(2*M*h-e));
 
 %Compute Depth Magnification 
 MD = (M*V*e*f*t)/(M*f*t-z0*(2*M*h-e)).^2;
@@ -39,3 +39,5 @@ MD = (M*V*e*f*t)/(M*f*t-z0*(2*M*h-e)).^2;
 %Compute Shape Ratio
 S = (MD/Mi3D);
 
+a = (M*f*t-z0*(2*M*h-e)).^2;
+b=(M*V*e*f*t);
